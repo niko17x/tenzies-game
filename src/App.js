a7855aaf213018e4ba728a93a2905bd1824bca91
Niko Shin <nshin17@gmail.com>
2023-02-23 12:27:38 -0800

    Fixed bug causing Best Time to update time even if time was not a better score. Now time updates only if new time is lower/better.

--- src/App.js
+++ src/App.js
@@ -7,14 +7,13 @@ import Confetti from "react-confetti"; // Confetti effects dependency.
 const { v4: uuidv4 } = require("uuid"); // Unique id generator.
 
 function App() {
-  const [dice, setDice] = React.useState(createDiceSet());
-  const [rolls, setRolls] = React.useState(0);
-  const [tenzies, setTenzies] = React.useState(false); // Track game win.
   // Best score for local storage.
   const [bestTime, setBestTime] = React.useState(
     JSON.parse(localStorage.getItem("bestTime") || 0)
   );
-
+  const [dice, setDice] = React.useState(createDiceSet());
+  const [rolls, setRolls] = React.useState(0);
+  const [tenzies, setTenzies] = React.useState(false); // Track game win.
   const [timer, setTimer] = React.useState(0);
   const [isActive, setIsActive] = React.useState(false);
   const [isPaused, setIsPaused] = React.useState(false);
@@ -30,6 +29,8 @@ function App() {
   }, [dice]);
 
   React.useEffect(() => {
+    console.log("logged");
+    displayWinText();
     utilGetBestTime();
     clearInterval(countRef.current);
     setIsPaused(false);
@@ -38,6 +39,26 @@ function App() {
   React.useEffect(() => {
     localStorage.setItem("bestTime", JSON.stringify(bestTime));
   }, [bestTime]);
+
+  // ? BUG => best time and current time are always going to equal b/c the change in value is currently dependent to each other :
+  function displayWinText() {
+    let message =
+      "Roll until all dice are the same. Click each die to freeze it at its current value between rolls.";
+
+    const currentTime = document.querySelector(
+      ".tracker--timer > .tracker--props"
+    );
+
+    if (tenzies && currentTime.textContent) {
+      if (parseInt(currentTime.textContent) < parseInt(bestTime)) {
+        message = "Awesome! You've set a new best time!";
+      } else {
+        message = "You Win!";
+      }
+    }
+    console.log(parseInt(currentTime) === parseInt(bestTime));
+    return message;
+  }
 
   function createDiceSet() {
     // get a random object from dieData and push that 10 times into a new array.
@@ -50,8 +71,6 @@ function App() {
 
   function generateNewDie() {
     const randomGenerator = Math.floor(Math.random() * 6);
-    const uniqueIdGenerator =
-      Math.floor(Math.random() * 9999) * Date.now().toString();
     // Adding objects from dieData.js and adding 'id' property to each object with unique id :
     const newDie = { ...dieData[randomGenerator], id: uuidv4() };
     return newDie;
@@ -100,8 +119,8 @@ function App() {
       setDice(createDiceSet());
       setRolls(0);
       setTimer(0);
-      // utilGetBestTime();
     }
+    // foo();
   }
 
   function getDieUrl(value) {
@@ -120,6 +139,16 @@ function App() {
     }
   }
 
+  function foo() {
+    setDice((prevDie) =>
+      prevDie.map((die) => {
+        return die.isHeld === false && die.value === 1
+          ? (die.isHeld = true)
+          : { ...die };
+      })
+    );
+  }
+
   // Create new Die.js component with props passed through :
   const diceElements = dice.map((die) => (
     <Die
@@ -136,14 +165,7 @@ function App() {
       {tenzies && <Confetti />}
       <div className="app--header">
         <h1>TENZIES</h1>
-        {tenzies ? (
-          <p>You Win!</p>
-        ) : (
-          <p>
-            Roll until all dice are the same. Click each die to freeze it at its
-            current value between rolls.
-          </p>
-        )}
+        <p>{displayWinText()}</p>
       </div>
       <Tracker diceRoll={rolls} bestTime={bestTime} timer={timer} />
       <div className="die--container">{diceElements}</div>
